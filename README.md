054c4a8c8c17a10b1c3bc498d9780a383bc645e4
Dejon Gill <dejonjgill@gmail.com>
2025-03-20 09:53:30 -0400

    minor update
    Signed-off-by: Dejon Gill <dejonjgill@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Home Lab Dashboard
+
+Running several services in my home lab, here is a convenient dashboard for
+navigating to the various services.
--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("GET request received"))
+}
+
 func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", getHandler).Methods(("GET"))
+
+	http.ListenAndServe(":8080", r)
 }
